ef6fcf96e91d0961950dccd5295d952a5500d332
Olga Polikarpova <olga.polikarpova@erlyvideo.org>
2022-10-17 18:03:25 +0400

    visual test tolerance 0.8

--- cypress/e2e/toc-appearance.cy.js
+++ cypress/e2e/toc-appearance.cy.js
@@ -1,7 +1,8 @@
 import ui from '../support/ui';
 
-// change tolerance to 0 to make tests fail
-const tolerance = 0;
+// change tolerance to 0 to make visual tests fail
+// comparison results are located in cypress-visual-screenshots/
+const tolerance = 0.8;
 const retryOptions = { limit: 3, delay: 500 };
 
 describe('Test TOC appearance on page run-for-the-first-time.html', () => {
